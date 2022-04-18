586de046aabb1a084d8be7289f1c23661b5bdc1d
JustineM98 <justinemugishe98@gmail.com>
2022-04-18 23:50:35 +0200

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
+ */
+int _printf(char *format, ...)
+{
+	int written = 0, (*structype)(char *, va_list);
+	char q[3];
+	va_list pa;
+
+	if (format == NULL)
+		return (-1);
+	q[2] = '\0';
+	va_start(pa, format);
+	_putchar(-1);
+	while (format[0])
+	{
+		if (format[0] == '%')
+		{
+			structype = driver(format);
+			if (structype)
+			{
+				q[0] = '%';
+				q[1] = format[1];
+				written += s
+				}

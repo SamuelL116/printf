13b06c61bc38bb5edb1bd2c4e1f299718397a1f9
JustineM98 <99365182+JustineM98@users.noreply.github.com>
2022-04-23 00:55:23 +0200

    Update _puts.c

--- _puts.c
+++ _puts.c
@@ -1,20 +1,38 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * new line, to stdout.
- * @string: input string
- * Return: count of string.
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return: void
  */
-int _puts(char *string)
+int _puts(char *str)
 {
-int contador = 0;
+	char *a = str;
 
-while (*string)
-{
-_putchar(*string);
-string++;
-contador++;
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
 }
-return (contador);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
